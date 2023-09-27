91370b8b2ac765a96905fb8fa55c3e1bbb4e1c8e
Pepe <polopepe567@gmail.com>
2023-09-27 19:10:56 -0400

    null.vier

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+#![allow(non_snake_case)]
+
+//single line comment
+
+/*
+
+
+*/ 
+
+
 use std:: {i8, i16, i32, u8, u16, u32, u64, isize, usize,
 f32, f64};
 
@@ -9,4 +19,9 @@ fn main() {
 
     let num: i8 = 10;
     println!("the number is {}", num);
+
+    let name: &str = "Bek";
+    let sur_name: &str = "Brace";
+    println!("My name is {} {}", name, sur_name )
+
 }
