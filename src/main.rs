7b0d840318e973dea82ef9648ba7b632a6dd95a8
Pepe <polopepe567@gmail.com>
2023-09-27 19:00:54 -0400

    null.drei

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std:: {i8, i16, i32, u8, u16, u32, u64, isize, usize,
+f32, f64};
+
+use std::io::stdin;
+
+
 fn main() {
     println!("Hello, world!");
+
+    let num: i8 = 10;
+    println!("the number is {}", num);
 }
